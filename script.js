5e4dad0ecbf79a5bd08b4901ef4405f2a185697c
nicolasduartesousa-tech <nicolas.duarte.sousa@escola.pr.gov.br>
2026-08-28 08:12:43 -0300

    a

--- script.js
+++ script.js
@@ -15,3 +15,13 @@ botoes.forEach(function (botao) {
         }
     }
 });
+
+function mudaTema() {
+    const corpoPagina = document.body;
+
+    if (corpoPagina.classList.contains("tema-escuro")) {
+        corpoPagina.classList.remove("tema-escuro");
+    } else {
+        corpoPagina.classList.add("tema-escuro");
+    }
+}
