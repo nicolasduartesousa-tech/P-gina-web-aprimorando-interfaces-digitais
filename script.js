d919e057798b2626377fc73a694d5926a6ef9a90
nicolasduartesousa-tech <nicolas.duarte.sousa@escola.pr.gov.br>
2026-09-01 08:21:07 -0300

    a

--- script.js
+++ script.js
@@ -18,13 +18,13 @@ botoes.forEach(function (botao) {
 
 const btnTemaEscuro = document.querySelector(".btn-Tema-escuro");
 
- btnTemaEscuro.addEventListener("click", mudaTema);
+btnTemaEscuro.addEventListener("click", mudaTema);
 
- function mudaTema() {
-  const corpoPagina = document.body
-  if (corpoPagina.classList.contains("tema escuro")){
+function mudaTema() {
+  const corpoPagina = document.body;
+  if (corpoPagina.classList.contains("tema-escuro")) {
       corpoPagina.classList.remove("tema-escuro");
   } else {
       corpoPagina.classList.add("tema-escuro");
   }
- }
+}
